568efc48b695962da378d04a4167d7b1ab908e46
Pavel dub <dubstr1@gmail.com>
2020-03-31 18:31:07 +0300

    angryBirds physics

--- js/angular-projection.js
+++ js/angular-projection.js
@@ -10,23 +10,39 @@ document.addEventListener('DOMContentLoaded', function () {
   ball1.y = 50
   ball1.context = context
 
+  // Ball 2
+  let ball2 = new Ball(20, 'green')
+  ball2.x = 90
+  ball2.y = 550
+  ball2.context = context
+
   window.requestAnimationFrame(animationLoop)
 
   // velocity
   ball1.vx = 5
   ball1.vy = 0
 
+  ball2.vx = 4
+  ball2.vy = -9
+
   function animationLoop () {
     // clear canvas
     context.clearRect(0, 0, canvas.width, canvas.height)
 
     // update
+    // Ball 1
     ball1.vy += g
     ball1.x += ball1.vx
     ball1.y += ball1.vy
 
+    // Ball 2
+    ball2.vy += g
+    ball2.x += ball2.vx
+    ball2.y += ball2.vy
+
     // draw
     ball1.draw()
+    ball2.draw()
 
     // Animate
     window.requestAnimationFrame(animationLoop)
